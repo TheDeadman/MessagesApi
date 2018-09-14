ad91ba832bd6979563cee50fcfbe846e02fb71ce
TheDeadman <AlexDeadmanLindemann@gmail.com>
2018-09-14 04:15:44 -0500

    Update notification-default.js

--- data/notification-default.js
+++ data/notification-default.js
@@ -1,7 +1,7 @@
 let notificationDefaultData = [
     {
-        title: "test1",
-        shortText: "this is a test",
+        title: "Overdue TimeCard",
+        shortText: "Please enter your timecard in clarity ppm.",
         longText: "This is the long part of the message!",
         image: "https://cdn.shopify.com/s/files/1/0747/3829/products/mHP1549_1024x1024.jpeg",
         id: '71e3e4fe-eba4-4323-b893-469622ba8502',
@@ -12,27 +12,6 @@ let notificationDefaultData = [
         longText: "This is the long part of the message!",
         image: "https://cdn.shopify.com/s/files/1/0747/3829/products/mHP1549_1024x1024.jpeg",
         id: 'bee72148-6aaa-4788-8b08-559842b9cdd6',
-        dismissed: false
-    },{
-        title: "test3",
-        shortText: "this is a test",
-        longText: "This is the long part of the message!",
-        image: "https://cdn.shopify.com/s/files/1/0747/3829/products/mHP1549_1024x1024.jpeg",
-        id: '9ed15981-fdda-420b-8606-7c3519a0ba61',
-        dismissed: false
-    },{
-        title: "test4",
-        shortText: "this is a test",
-        longText: "This is the long part of the message!",
-        image: "https://cdn.shopify.com/s/files/1/0747/3829/products/mHP1549_1024x1024.jpeg",
-        id: '83717a78-be53-4f02-98dc-106c9737af87',
-        dismissed: false
-    },{
-        title: "test5",
-        shortText: "this is a test",
-        longText: "This is the long part of the message!",
-        image: "https://cdn.shopify.com/s/files/1/0747/3829/products/mHP1549_1024x1024.jpeg",
-        id: 'ba9b0f5d-4aad-4376-976c-43c12c535555',
         dismissed: false
     },
 ]
